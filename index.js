5efd1ca58c793d37049c632d1f833c4216a48704
Elizabeth <ehernandez@alumnos.uaq.mx>
2025-03-14 00:15:40 -0600

    cambios que se realizaron

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const express = require ('express');
 const app = express();
-const { pokemon } = require('./pokedex.json');
+const pokemon = require('./routes/pokemon');
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,35 +13,9 @@ app.get ("/", (req, res, next) => {
    return res.status(200).send("Bienvenido al Pokedex");
 });
 
-app.post ("/pokemon", (req, res, next) => {
-    return res.status(200).send(req.body);
- });
- 
+app.use("/pokemon", pokemon);
 
-app.get ('/pokemon', (req, res, next) => {
-    return res.status(200).send(pokemon);
-});
 
-app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
-    const id = req.params.id - 1;
-    if (id >= 0 && id <= 150) {
-       return res.status(200).send(pokemon[req.params.id - 1])
-    }
-    return  res.status(404).send("Pokemon no encontrado");
-});
-
-app.get('/pokemon/:name([A-Za-z]+)', (req, res, next) => {
-    const name = req.params.name;
-
-    const pk = pokemon.filter((p) => {
-        return (p.name.toUpperCase() == name.toUpperCase ()) ? p : null;
-    }); 
-
-    if(pk.length > 0) {
-        return res.status(200).send(pk);
-    }
-    return res.status(404).send("Pokemon no encontrado");
-});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
